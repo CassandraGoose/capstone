87eeb5be8534125dfa9b4fb35be7d2b7917cfa73
Cassandra Torske <cassandra.torske@gmail.com>
2017-03-20 17:47:27 -0600

    housekeeping

--- routes/images.js
+++ routes/images.js
@@ -33,7 +33,7 @@ router.get('/images', (req, res, next) => {
 })
 
 router.get('/user/:id/collection/', authMiddleware.allowAccess, (req, res, next) => {
-  knex('collected_images').where(req.params.id, 'user_id')
+  knex('collected_images').where(req.params.id, 'collected_images.user_id')
     .then(collected_images => res.json(collected_images))
 })
 
@@ -51,68 +51,5 @@ router.post('/upload', (req, res, next) => {
     .then(uploaded_images => res.json(uploaded_images[0]))
     .catch(err => next(err))
 })
-
-
-//for the user thing but i'm not sure how because he used queries
-
-
-
-// .insert(params(req))
-//     .returning('*')
-
-// router.patch('/:id', validate, (req, res, next) => {
-//   knex('posts')
-//     .update(params(req))
-//     .where({id: req.params.id})
-//     .returning('*')
-//     .then(posts => res.json(posts[0]))
-//     .catch(err => next(err))
-// })
-
-// router.delete('/:id', (req, res, next) => {
-//   knex('posts')
-//     .del()
-//     .where({id: req.params.id})
-//     .then(() => res.end())
-//     .catch(err => next(err))
-// })
-//
-// router.post('/:id/votes', (req, res, next) => {
-//   knex('posts')
-//     .update('vote_count', knex.raw('vote_count + 1'))
-//     .where({id: req.params.id})
-//     .then( () => knex('posts').where({id: req.params.id}).first() )
-//     .then( post => res.json({vote_count: post.vote_count}))
-//     .catch(err => next(err))
-// })
-//
-// router.delete('/:id/votes', (req, res, next) => {
-//   knex('posts')
-//     .update('vote_count', knex.raw('vote_count - 1'))
-//     .where({id: req.params.id})
-//     .then( () => knex('posts').where({id: req.params.id}).first() )
-//     .then( post => res.json({vote_count: post.vote_count}))
-//     .catch(err => next(err))
-// })
-//
-// function params(req) {
-//   return {
-//     title: req.body.title,
-//     body: req.body.body,
-//     author: req.body.author,
-//     image_url: req.body.image_url,
-//   }
-// }
-//
-// function validate(req, res, next) {
-//   const errors = [];
-//   ['title', 'body', 'author', 'image_url'].forEach(field => {
-//     if (!req.body[field] || req.body[field].trim() === '') {
-//       errors.push({field: field, messages: ["cannot be blank"]})
-//     }
-//   })
-//   if (errors.length) return res.status(422).json({errors})
-//   next()
-// }
 
 module.exports = router
